b18c63e334dc9fb908a365ca9141322ce1e00a31
ghanshyam-du <ghanshyamd816@gmail.com>
2025-09-11 18:44:23 +0530

    Define how user data will be stored

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectMongodb from "./connection.js"
+import user from "./model/user.js"
 
 const app = express();
 dotenv.config();
@@ -15,3 +16,17 @@ app.listen(PORT, ()=>{
     console.log(`Server is running successfuly at the ${PORT}`);
 })
 
+const createDummyUser = async () => {
+  try {
+    const User = new user({
+      username: "ghanshyam",
+      password: "123456"   // ⚠️ plaintext only for testing, later we will hash
+    });
+    await User.save();
+    console.log("✅ Dummy user created:", User);
+  } catch (err) {
+    console.log("❌ Error creating dummy user:", err.message);
+  }
+};
+
+createDummyUser();
