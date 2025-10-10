651f74f363180fc5fffa3e6d4ecbd6eeac566645
ghanshyam-du <ghanshyamd816@gmail.com>
2025-10-10 23:26:30 +0530

    updated

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -22,7 +22,7 @@ export const register = async (req, res)=>{
         const newUser = new User({ username, password: hashPass });
         await newUser.save();
 
-        res.status(201).json({ message: "User registered successfully!" });
+        res.status(201).json({ message: "User registered successfully! " });
     } catch (err) {
         res.status(400).json({ error: "something went wrong :", details: err.message })
     }
