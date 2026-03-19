af0874ce5388629f4fbd9a5dfd9136eddc1b03b4
ghanshyam-du <ghanshyamd816@gmail.com>
2026-03-19 22:51:48 +0530

    updated the connection code

--- connection.js
+++ connection.js
@@ -7,5 +7,4 @@ export default async function connectMongodb() {
     mongoose.connect(mongo_url)
     .then(()=>{console.log("Mongodb is connected successfuly!")})
     .catch((err)=>{console.log("connection error:"+err)});
-    
 }
